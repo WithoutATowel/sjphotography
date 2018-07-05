682a293a9513fe136049a2602542f35dde97562f
Brett <brettspencr@gmail.com>
2018-07-05 14:35:53 -0700

    Adding an allowed host and turning debug mode off

--- sjspencer/settings.py
+++ sjspencer/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&3a@%!*@y%rawyv7#vkk=_fy!*1nyy+rw!bc2-2tew8s4k2_&n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.2sjspencer.net']
 
 # Application definition
 
